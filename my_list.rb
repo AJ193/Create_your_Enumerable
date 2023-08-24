02476cdbe13b26cfdcd627d364f8bbd194d3ae6b
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-08-24 15:09:55 +0500

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,17 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*elements)
+    @list = elements
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
 # Create our list
 list = MyList.new(1, 2, 3, 4)
 
