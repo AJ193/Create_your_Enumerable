728194909881f98db8ef769e3751cda9111f6dbf
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-08-24 13:09:01 +0500

    Update my_list.rb file

--- my_list.rb
+++ my_list.rb
@@ -1,17 +1,27 @@
-module MyEnumerable
-  def all?(&block)
-    each { |element| return false unless block.call(element) }
-    true
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*elements)
+    @list = elements
   end
 
-  def any?(&block)
-    each { |element| return true if block.call(element) }
-    false
-  end
-
-  def filter(&block)
-    result = []
-    each { |element| result << element if block.call(element) }
-    result
+  def each(&block)
+    @list.each(&block)
   end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # Output: true
+puts(list.all? { |e| e > 5 }) # Output: false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # Output: true
+puts(list.any? { |e| e == 5 }) # Output: false
+
+# Test #filter
+puts(list.filter(&:even?)) # Output: [2, 4]
